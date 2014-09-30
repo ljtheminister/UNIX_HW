d23532eb2551b5c96af9d74e60a5f97d0448a54b
ljtheminister <john.c.min@gmail.com>
2014-09-30 13:21:32 -0400

    finished hw1_1 and wrote out all the questions

---
+++ hw1.sh
@@ -0,0 +1,26 @@
+
+# Print the length of the absolute pathname of the current directory
+pwd | wc -c
+
+
+# Print the number of files in your directory
+ls | wc - l
+
+# Use find to set the creation time lf all the files in your directory to the current time
+
+# Create an empty file foo. Use octal modes to make the file nonexecutable and only readable and writable to you.
+
+# Print the list of the users logged on with the number of times each is logged on.
+
+
+# Create a file in the current directory containing exactly lines 2000-3999 of /etc/passwd, each line preceded with its line number in the original file
+
+
+# Use octal modes ot make the file non-executable, and only readable and writeable by you
+
+# Write the names of all users found in this file to users after replacing upper case letters into lower case ones
+
+# Print the 10 most frequent first names in users preceded with their number of occurences.
+
+# Sort /etc/passwd by the users' names and print the 15 last userids after removing all digits.
+
